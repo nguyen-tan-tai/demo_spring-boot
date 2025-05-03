a7aaf8082d673a26a108a2d315ee8e2c31a5b09d
Nguyen Tan Tai <tantantai@gmail.com>
2025-05-03 17:40:27 +0700

    Refactor Jackson utils

--- src/main/java/com/example/demo/commons/json/JacksonObjectMapperFactory.java
+++ src/main/java/com/example/demo/commons/json/JacksonObjectMapperFactory.java
@@ -4,11 +4,19 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
 public class JacksonObjectMapperFactory {
+
     public static ObjectMapper create() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(LocalDateTimeSerializer.INSTANCE);
+        module.addSerializer(LocalDateSerializer.INSTANCE);
+        module.addSerializer(LocalTimeSerializer.INSTANCE);
+        mapper.registerModule(module);
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return mapper;
