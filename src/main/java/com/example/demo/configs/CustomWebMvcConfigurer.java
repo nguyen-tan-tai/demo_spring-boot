67a9d8281e227e053a63be15156266f3659656e7
Nguyen Tan Tai <tantantai@gmail.com>
2025-05-03 17:40:27 +0700

    Refactor only

--- src/main/java/com/example/demo/configs/CustomWebMvcConfigurer.java
+++ src/main/java/com/example/demo/configs/CustomWebMvcConfigurer.java
@@ -1,18 +1,22 @@
 package com.example.demo.configs;
 
+import com.example.demo.interceptors.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.example.demo.interceptors.AuthInterceptor;
-
 @Configuration
 public class CustomWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    AuthInterceptor authInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthInterceptor());
+        registry.addInterceptor(authInterceptor);
     }
 
     @Override
