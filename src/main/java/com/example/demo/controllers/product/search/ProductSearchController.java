78c2f57dfb655914fabf2a3d679f3f4db3577806
Nguyen Tan Tai <tantantai@gmail.com>
2025-05-21 17:10:02 +0700

    Remove @RequestBody because of get

--- src/main/java/com/example/demo/controllers/product/search/ProductSearchController.java
+++ src/main/java/com/example/demo/controllers/product/search/ProductSearchController.java
@@ -3,11 +3,11 @@ package com.example.demo.controllers.product.search;
 import com.example.demo.controllers.BaseController;
 import com.example.demo.models.entities.Product;
 import com.example.demo.models.repositories.ProductRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +17,7 @@ public class ProductSearchController extends BaseController {
     private ProductRepository productRepository;
 
     @GetMapping("/products")
-    public ResponseEntity<?> search(@RequestBody ProductSearchRequest request) {
+    public ResponseEntity<?> search(@Valid ProductSearchRequest request) {
         Page<Product> products = productRepository.seach(request.getProductName(), request.toPageable());
         return ok(new ProductSearchResponse(products));
     }
